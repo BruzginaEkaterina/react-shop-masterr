e7b80e56943856246c8ea94b4434d621fb486398
Ekaterina Bruzgina <bruzgina.ek@gmail.com>
2023-03-22 14:22:26 +0300

    feat: add project

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export function Footer() {
+    return (
+        <footer className='bg-black text-white'>
+            <div className='max-w-[1240px] mx-auto py-2 text-sm sm:text-base'>
+                <div className='flex justify-center'>
+                    
+                </div>
+                <div className='flex justify-center flex-col items-center'>
+                    <p className='text-center'>© Klieron</p>
+                    <a>Created by Ekaterina Bruzgina</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
